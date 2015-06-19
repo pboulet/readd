55bbbb1c595540387ccfb5e2f62cdda41671d26c
pboulet <pboul037@gmail.com>
2015-06-18 21:47:51 -0400

    added category & search inputs on navigation bar & chosen plugin

--- public/js/Bookshelf.js
+++ public/js/Bookshelf.js
@@ -293,35 +293,16 @@ var Bookshelf = (function ($this){
           "hideMethod": "fadeOut"
         };
         
+        // configure & initialize chosen
+        $(".chosen-select").chosen({no_results_text: "Oops, nothing found!"}); 
+        
         initViewModel();
         
         // Closes the Responsive Menu on Menu Item Click
         $('.navbar-collapse ul li a').click(function() {
             $('.navbar-toggle:visible').click();
         });
-
-//  scrap code from orgininal wizard template
-//        allNextBtn.click(function(){
-//            var curStep = $(this).closest(".setup-content"),
-//                curStepBtn = curStep.attr("id"),
-//                nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-//                curInputs = curStep.find("input[type='text'],input[type='url']"),
-//                isValid = true;
-//
-//            $(".form-group").removeClass("has-error");
-//            for(var i=0; i<curInputs.length; i++){
-//                if (!curInputs[i].validity.valid){
-//                    isValid = false;
-//                    $(curInputs[i]).closest(".form-group").addClass("has-error");
-//                }
-//            }
-//
-//            if (isValid)
-//                nextStepWizard.removeAttr('disabled').trigger('click');
-//        });
-
-//        $('div.setup-panel div a.btn-primary').trigger('click');
-
+        
         $this.vm = vm;
         
         ko.applyBindings(vm);
